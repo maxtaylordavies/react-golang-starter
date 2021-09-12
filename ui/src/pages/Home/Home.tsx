18f1deec9524cac3b417089328767f7845e640d9
Max Taylor-Davies <maxtaylordavies@gmail.com>
2021-09-12 22:52:02 +0100

    initial commit

---
+++ ui/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <motion.div className="home-page">
+      <div>HOME</div>
+    </motion.div>
+  );
+};
